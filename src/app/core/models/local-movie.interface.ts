a05cfd8b276df7d87774229e78ee005ff0a9b2e3
Andres Munive <fmunive@getontop.com>
2023-12-05 07:41:41 -0500

    Set local movie data

---
+++ src/app/core/models/local-movie.interface.ts
@@ -0,0 +1,11 @@
+export interface LocalMovieModel {
+  title: string;
+  description: string;
+  rating: number;
+  duration: string;
+  genre: string[];
+  released_date: string;
+  trailer_link: string;
+  poster: string;
+  backdrop: string;
+}
